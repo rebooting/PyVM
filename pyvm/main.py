4f615f4a96ba9a11f2a56a6f681cfc723f057c8e
alvin <rebooting@gmail.com>
2026-04-04 00:30:20 +0800

    added comments to describe the program is supposed to run

--- pyvm/main.py
+++ pyvm/main.py
@@ -2,7 +2,12 @@
 from .vm import ToyVM
 
 if __name__ == "__main__":
+    # example program to move 69 into R0 and print it
+    # mov R0, 69
+    # print R0
+    # halt
     prog = [4, 0, 69, 3, 0, 99]
+
     toy_vm = ToyVM()
     toy_vm.load(prog)
     toy_vm.run()
